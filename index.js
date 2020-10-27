54004b7bcf300c6cc7b6995207f6feeed5ed9425
Qian Dong <qdong327@gmail.com>
2020-10-26 22:36:31 -0400

    added connections

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // Dependencies
 const inquirer = require("inquirer");
 var mysql = require("mysql");
+
+// Database Connection
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "NoriskNoreward1",
+    database: "company_db"
+});
